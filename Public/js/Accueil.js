b4f02be9b7af27c5292062df6075fa677834fbbf
Dasten-Duranin <dasten.duranin@gmail.com>
2015-01-19 17:08:49 +0100

    Update du 19/01/2015 17h08
    Avancée de la partie élève de l'accueil.

--- Public/js/Accueil.js
+++ Public/js/Accueil.js
@@ -2,16 +2,67 @@ window.addEventListener("load",initialisation,false); /*Ne pas exectuer code tan
 
 function initialisation (TypeDomaine) {
 
-    $('.DomaineDevOf .SubDel:first').css({'display' : 'none'});
-    $('.DomaineResOf .SubDel:first').css({'display' : 'none'});
-    $('.DomaineDevModifOf .SubDel:first').css({'display' : 'none'});
-    $('.DomaineResModifOf .SubDel:first').css({'display' : 'none'});
+    disp_eleves(2);
     start();
 
 };
 
 function start () {
 
-    var TypeDomaine =0;
-    var idOffre =0;
+    var numPage =2;
+    console.log(numPage);
+
+
+    $('.ArrowEleve').click(function() {
+        if ($(this)[0].id == 'ArrowEleveLeft') {
+            console.log($(this)[0].id);
+            if (numPage == 1) {numPage=4;}
+            numPage-=1;
+            console.log(numPage)
+            disp_eleves(numPage);
+        }
+        else {
+            console.log($(this)[0].id);
+            if (numPage == 3) {numPage=0;}
+            numPage+=1;
+            console.log(numPage)
+            disp_eleves(numPage);
+        }
+    });
+
+};
+
+function disp_eleves(numPage) {
+
+    var idEleve1;
+    var idEleve2;
+    var idEleve3;
+
+    $('.eleve').css({'display' : 'none'});
+
+    switch(numPage) {
+        case 1:
+            idEleve1 = 1;
+            idEleve2 = 2;
+            idEleve3 = 3;
+            break;
+        case 2:
+            idEleve1 = 4;
+            idEleve2 = 5;
+            idEleve3 = 6;
+            break;
+        case 3:
+            idEleve1 = 7;
+            idEleve2 = 8;
+            idEleve3 = 9;
+            break;
+        default:
+            idEleve1 = 4;
+            idEleve2 = 5;
+            idEleve3 = 6;
+    }
+
+    $('#eleve'+idEleve1).css({'display' : 'block'});
+    $('#eleve'+idEleve2).css({'display' : 'block'});
+    $('#eleve'+idEleve3).css({'display' : 'block'});
 };
--- application/views/view_Accueil.php
+++ application/views/view_Accueil.php
@@ -1,14 +1,22 @@
 <section class="clear">
     <article class="eleves clear">
+        <img class="ArrowEleve" src="<?php echo base_url();?>Public\img\Interface\ArrowLeft.png" alt="Fleche de Gauche" title="Fleche de Gauche" id="ArrowEleveLeft" />
         <?php $i=0;
             foreach ($eleves as $eleve) { $i+=1; echo '
                 <div class="eleve" id="eleve'.$i.'">
-                    <img src="'; if($eleve->PhotoProfil != FALSE) {echo $eleve->PhotoProfil;} else {echo base_url().'Public/img/Defaut_Photo_Profile.png';} echo '" width="210" height="270" />
+                    <div class="Names">
+                        '.$eleve->Nom.'<br>
+                        '.$eleve->Prenom.'
+                    </div>
+                    <a href="'.base_url().'Profil/Index?idEleve='.$eleve->idEleve.'">
+                        <img src="'; if($eleve->PhotoProfil != FALSE) {echo $eleve->PhotoProfil;} else {echo base_url().'Public/img/Defaut_Photo_Profile.png';} echo '" width="210" height="270" />
+                    </a>
                     <div class="accroche">
                         '.$eleve->Accroche.'
                     </div>
                 </div>
             ';}
         ?>
+        <img class="ArrowEleve" src="<?php echo base_url();?>Public\img\Interface\ArrowRight.png" alt="Fleche de Gauche" title="Fleche de Gauche" id="ArrowEleveRight" />
     </article>
 </section>
